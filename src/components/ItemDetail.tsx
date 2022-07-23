e69c9ec284c3ef161556f5e47bc63e9a696cab11
Dave Degeatano <daveydee33@users.noreply.github.com>
2022-07-23 18:07:17 +0800

    improve item details page, layout

--- src/components/ItemDetail.tsx
+++ src/components/ItemDetail.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Chip, Typography } from "@mui/material";
 import { useItemContext } from "../hooks/useItemContext";
 
 const ItemDetail = () => {
@@ -15,17 +15,82 @@ const ItemDetail = () => {
         // alignContent: "flex-start",
         backgroundColor: "secondary.main",
         width: "400px",
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
       }}
     >
       {/* <Typography variant="h6">Item Detail</Typography> */}
-      <Typography variant="h6" component="h1">
-        {selectedItem.title}
-      </Typography>
-      <Typography variant="subtitle1">{selectedItem.description}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "baseline",
+          gap: 2,
+        }}
+      >
+        <Typography variant="h6" component="h1">
+          {selectedItem.title}
+        </Typography>
+        <Typography variant="subtitle1">{selectedItem.description}</Typography>
+      </Box>
       <Typography variant="subtitle2">{selectedItem.details}</Typography>
-      <Typography>{JSON.stringify(selectedItem.examples)}</Typography>
-      <Typography>{JSON.stringify(selectedItem.related)}</Typography>
-      <Typography>{JSON.stringify(selectedItem.seeAlso)}</Typography>
+      {/* <Typography>{JSON.stringify(selectedItem.examples)}</Typography> */}
+
+      <div>
+        <Typography>Examples:</Typography>
+        {selectedItem.examples &&
+          selectedItem.examples.map((example) => (
+            <div
+              key={(example.title, example.description)}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "1rem",
+              }}
+            >
+              <div>{example.title}</div>
+              <div>
+                <i>{example.description}</i>
+              </div>
+            </div>
+          ))}
+      </div>
+
+      <div>
+        <Typography>Tags: </Typography>
+        {/* <Typography>{JSON.stringify(selectedItem.tags)}</Typography> */}
+        {selectedItem.tags &&
+          selectedItem.tags
+            .filter((v) => v)
+            .map((word) => (
+              <Chip
+                key={word}
+                label={word}
+                variant="outlined"
+                color="info"
+                size="medium"
+                sx={{ m: 0.5 }}
+              />
+            ))}
+      </div>
+
+      <div>
+        <Typography>Related: </Typography>
+        {/* <Typography>{JSON.stringify(selectedItem.related)}</Typography> */}
+        {selectedItem.related &&
+          selectedItem.related
+            .filter((v) => v)
+            .map((word) => <Chip label={word} variant="filled" color="info" />)}
+      </div>
+
+      <div>
+        <Typography>See Also: </Typography>
+        {/* <Typography>{JSON.stringify(selectedItem.seeAlso)}</Typography> */}
+        {selectedItem.seeAlso?.map((word) => (
+          <Chip label={word} variant="outlined" color="primary" size="medium" />
+        ))}
+      </div>
+
       {/* <Typography>ID: {selectedItem.id}</Typography> */}
       {/* <Typography>Audio: {selectedItem.audio}</Typography> */}
       {/* <Typography>Images: {selectedItem.images}</Typography> */}
